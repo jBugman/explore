fe7a335a85f5fc1fa3f42dc686c3122bf84e1221
Sergey Parshukov <sergey.parshukov@me.com>
2015-05-21 17:09:47 +0300

    Reading filed from found JSON files

--- process.rs
+++ process.rs
@@ -1,6 +1,15 @@
-use std::env;
+extern crate glob;
+extern crate rustc_serialize;
 
-const OUTPUT_FILE: &'static str = "output.csv";
+use glob::glob;
+use rustc_serialize::json::Json;
+use std::env;
+use std::fs::File;
+use std::result::Result;
+use std::io::Read;
+
+
+// const OUTPUT_FILE: &'static str = "output.csv";
 
 fn main() {
 	let args: Vec<String>= env::args().collect();
@@ -11,5 +20,20 @@ fn main() {
 
 	let field = &args[1];
 	let folder = &args[2];
-	println!("Will process field '{}' in files located in {}", field, folder);
+	// println!("Will process field '{}' in files located in {}", field, folder);
+
+	let search_path = format!("{}/*.json", folder);
+	for path in glob(&search_path).unwrap().filter_map(Result::ok) {
+		// println!("{}", path.display());
+		let mut file = File::open(&path).unwrap();
+		let mut contents = &mut String::new();
+		file.read_to_string(contents).ok().expect("Failed to read file");
+
+		let data = Json::from_str(&contents).unwrap();
+		let value = data.find(field).expect("Field is missing");
+		match value.as_string() {
+			Some(_) => (),
+			None => panic!("Field is not string"),
+		}
+	}
 }
