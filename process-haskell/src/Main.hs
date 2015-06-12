d62453a8a00b008eb13c3dc587ff5204cd2d8fe9
Sergey Parshukov <sergey.parshukov@me.com>
2015-06-13 02:16:43 +0300

    Filtering list

--- process-haskell/src/Main.hs
+++ process-haskell/src/Main.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
 import System.Environment
 import System.FilePath.Glob
-import Data.Aeson
+import Data.Aeson (decode)
 import Data.Aeson.Types
 import Data.Text (pack, unpack, Text)
 import Data.ByteString.Lazy as B (readFile, ByteString)
@@ -18,13 +19,16 @@ process :: String -> String -> IO ()
 process field folder = do
     -- putStrLn ("Processing field '" ++ field ++ "' in " ++ folder)
     files <- globDir1 (compile "*.json") folder
-    mapM_ (process' field) files
-  where
-    process' field filename = do
-        contents <- B.readFile filename
-        let json = parseJson contents
-        let value = getFieldValue json field
-        putStrLn value
+    values <- mapM (process' field) files
+    let notEmpty = [x | x <- values, x /= ""]
+    mapM_ putStrLn $ notEmpty
+
+process' :: String -> String -> IO String
+process' field filename = do
+    contents <- B.readFile filename
+    let !json = parseJson contents
+    let value = getFieldValue json field
+    return value
 
 parseJson :: ByteString -> Object
 parseJson src = case (decode src :: Maybe Object) of
