ca5e68128da6b91e5d2fe6d4134ba799375afc71
Sergey Parshukov <sergey.parshukov@me.com>
2015-06-13 01:23:29 +0300

    Parsing JSON

--- process-haskell/src/Main.hs
+++ process-haskell/src/Main.hs
@@ -2,45 +2,39 @@ module Main where
 import System.Environment
 import System.FilePath.Glob
 import Data.Aeson
-import Data.ByteString.Lazy as BS (readFile, ByteString)
-import Data.HashMap as M (lookup, Map)
--- import Data.ByteString.Lazy.UTF8 as BS (fromString, toString)
-
+import Data.Aeson.Types
+import Data.Text (pack, unpack, Text)
+import Data.ByteString.Lazy as B (readFile, ByteString)
+-- import Data.HashMap as M (lookup, Map)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    field:folder:_ -> process field folder
-    _ -> error "Args are: <field name> <folder>"
-
+    args <- getArgs
+    case args of
+        field:folder:_ -> process field folder
+        _ -> error "Args are: <field name> <folder>"
 
 process :: String -> String -> IO ()
 process field folder = do
-  -- putStrLn ("Processing field '" ++ field ++ "' in " ++ folder)
-  files <- globDir1 (compile "*.json") folder
-  mapM_ (process' field) files
-
-process' :: String -> FilePath -> IO ()
-process' field filename = do
-  contents <- BS.readFile filename
-  -- let contents = BS.fromString "{\"test\": \"x\"}"
-  let json = parseJson contents
-  print $ getFieldValue json field
+    -- putStrLn ("Processing field '" ++ field ++ "' in " ++ folder)
+    files <- globDir1 (compile "*.json") folder
+    mapM_ (process' field) files
+  where
+    process' field filename = do
+        contents <- B.readFile filename
+        let json = parseJson contents
+        let value = getFieldValue json field
+        putStrLn value
 
 parseJson :: ByteString -> Object
 parseJson src = case (decode src :: Maybe Object) of
-  Just v -> v
-  Nothing -> error "Malformed JSON"
+    Just v -> v
+    Nothing -> error "Malformed JSON"
 
-getFieldValue :: M.Map String Value -> String -> String
+getFieldValue :: Object -> String -> String
 getFieldValue json field =
-  case (M.lookup field json :: Maybe Value) of
-  -- case (json .: field :: Parser Maybe String) of
-    Nothing -> error "Field is missing"
-    Just f -> case fromJSON f of
-      Error err -> error err
-      Success x -> x :: String
-
-    -- Just _ -> error "Field is not a string"
-    -- Nothing -> error "Field is missing"
+    case parse f json of
+        Error e -> error "Field is not a string"
+        Success Nothing -> error "Field is missing"
+        Success (Just x) -> unpack x
+      where f obj = obj .:? pack field :: Parser (Maybe Text)
