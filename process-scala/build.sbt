5afc7908bf8473262ca1046febbfd78f50f0210a
Sergey Parshukov <sergey.parshukov@me.com>
2015-06-24 01:11:10 +0300

    Scala implementation

---
+++ process-scala/build.sbt
@@ -0,0 +1,10 @@
+name := "process-scala"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+
+libraryDependencies ++= Seq(
+  "org.json4s" %% "json4s-native" % "3.2.10",
+  "com.github.tototoshi" %% "scala-csv" % "1.2.1"
+)
---
+++ process-scala/src/main/scala-2.11/Process.scala
@@ -0,0 +1,40 @@
+import java.io.{File, IOException}
+
+import com.github.tototoshi.csv.CSVWriter
+import org.json4s.JsonAST._
+import org.json4s.native.JsonMethods._
+
+import scala.collection.mutable
+import scala.io.Source
+
+object Process {
+
+  def main(args: Array[String]): Unit = args.toList match {
+    case field :: folder :: Nil => process(field, folder)
+    case _ => println("Args are: <field name> <folder>")
+  }
+
+  def process(field: String, folder: String): String = {
+    val frequencies = mutable.Map.empty[String, Int]
+
+    val files = new File(folder).listFiles().filter(x => x.isFile && x.getName.endsWith(".json"))
+    files foreach { file =>
+      val contents = Source.fromFile(file).mkString
+      val json = parse(contents)
+      json \ field match {
+        case JString("") =>
+        case JString(value) => frequencies(value) = frequencies.getOrElse(value, 0) + 1
+        case JNothing => throw new IOException("Field is missing")
+        case _ => throw new IOException("Field is not a string")
+      }
+    }
+    val sorted = frequencies.toList.sortWith(_._2 > _._2)
+
+    val outfile = new File("output.csv")
+    val writer = CSVWriter.open(outfile)
+    writer.writeAll(sorted.map(x => Seq(x._1, x._2.toString)))
+    writer.close()
+
+    "ok"
+  }
+}
