14d110c13bcfdda56ee094b4af164ea9547e7d3f
Sergey Parshukov <sergey.parshukov@me.com>
2015-07-16 19:20:29 +0300

    Forgot to save test

--- process-objc/ProcessTest/ProcessTest.m
+++ process-objc/ProcessTest/ProcessTest.m
@@ -1,13 +1,6 @@
-//
-//  ProcessTest.m
-//  ProcessTest
-//
-//  Created by Sergey Parshukov on 14.07.15.
-//  Copyright (c) 2015 Sergey Parshukov. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 #import <XCTest/XCTest.h>
+#import "process.h"
 
 @interface ProcessTest : XCTestCase
 
@@ -15,26 +8,9 @@
 
 @implementation ProcessTest
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testWorks {
+    int result = process(@"Name", @"../test_data/");
+    XCTAssert(result == EXIT_SUCCESS, @"It works");
 }
 
 @end
