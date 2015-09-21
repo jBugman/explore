aea076d6788ad0404f47b9b836feaf400cd2ae0e
Sergey Parshukov <sergey.parshukov@me.com>
2015-09-21 15:33:30 +0300

    C# NUnit test

---
+++ process-csharp/process_test.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ProcessCsharpTests {
+
+    [TestFixture()]
+    public class ProcessCsharpTest {
+
+        [Test()]
+        public void ShouldWork() {
+            Assert.AreEqual(ProcessCsharp.Process("Name", "../../test_data"), 0);
+        }
+    }
+}
