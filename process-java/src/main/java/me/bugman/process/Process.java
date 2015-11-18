749a8ff5211fa1a2bd0bd90ad3d5bf32713d1992
Sergey Parshukov <codedby@bugman.me>
2015-11-19 01:09:21 +0300

    Working app in a fat jar

--- process-java/src/main/java/me/bugman/process/Process.java
+++ process-java/src/main/java/me/bugman/process/Process.java
@@ -1,15 +1,14 @@
 package me.bugman.process;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.util.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 class Process {
-
-    private static String readFile(File f) throws IOException {
-        byte[] encoded = Files.readAllBytes(f.toPath());
-        return new String(encoded, StandardCharsets.UTF_8);
-    }
 
     public static int process(String field, String folder) {
         File dir = new File(folder);
@@ -19,17 +18,63 @@ class Process {
                 return name.toLowerCase().endsWith(".json");
             }
         });
+        HashMap<String, int[]> frequencies = new HashMap<String, int[]>();
+        ObjectMapper jsonMapper = new ObjectMapper();
         for(File file: files) {
-            // Read file as string
+            Map data;
+            // Read file as JSON
             String contents;
             try {
-                contents = readFile(file);
-            } catch (IOException ex) {
+                data = jsonMapper.readValue(file, Map.class);
+            } catch (Exception ex) {
                 System.err.println(ex.getMessage());
                 return 1;
             }
-            //
-            System.out.println(contents);
+            if (!data.containsKey(field)) {
+                System.err.println("Field is missing");
+                return 1;
+            }
+            // Check and record field
+            Object value = data.get(field);
+            if (value instanceof String) {
+                if (value == "") {
+                    continue;
+                }
+                int[] iw = frequencies.get(value);
+                if (iw == null) {
+                    frequencies.put((String)value, new int[] { 1 });
+                } else {
+                    iw[0]++;
+                }
+            } else {
+                System.err.println("Field is not a string");
+                return 1;
+            }
+        }
+        // Sort counter
+        List<Map.Entry<String, int[]>> sorted = new LinkedList<Map.Entry<String, int[]>>(frequencies.entrySet());
+        Collections.sort(sorted, new Comparator<Map.Entry<String, int[]>>() {
+            public int compare(Map.Entry<String, int[]> o1, Map.Entry<String, int[]> o2) {
+                Comparable<Integer> a = o2.getValue()[0];
+                int b = o1.getValue()[0];
+                return a.compareTo(b);
+            }
+        });
+        // Convert to string arrays
+        List<String[]> data = new LinkedList<String[]>();
+        for(Map.Entry<String, int[]> item: sorted) {
+//            System.out.printf("%s %d\n", item.getKey(), item.getValue()[0]);
+            data.add(new String[]{ item.getKey(), String.valueOf(item.getValue()[0]) });
+        }
+        // Write result as CSV
+        CsvMapper mapper = new CsvMapper();
+        mapper.enable(CsvParser.Feature.WRAP_AS_ARRAY);
+        File outFile = new File("output.csv");
+        try {
+            mapper.writeValue(outFile, data);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            return 1;
         }
         return 0;
     }
