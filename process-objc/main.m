4b9d2602c8d47650196786915287e5776157ca5e
Sergey Parshukov <codedby@bugman.me>
2015-11-23 23:28:37 +0300

    Much better Objective-C benchmark perfomance

--- process-objc/main.m
+++ process-objc/main.m
@@ -15,8 +15,8 @@ int main(int argc, const char * argv[]) {
 
 // // Benchmark
 // int main(int argc, const char * argv[]) {
-//     @autoreleasepool {
-//         for(int i = 0; i < 100; i++) {
+//     for(int i = 0; i < 100; i++) {
+//         @autoreleasepool {
 //             process(@"Name", @"../test_data/");
 //         }
 //     }
